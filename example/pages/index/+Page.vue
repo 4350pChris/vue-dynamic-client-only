1e7aa10571354825f20ef5df7e7dca8ca4aaf16e
Chris-Robin Ennen <chris-robin@ennen.dev>
2024-01-05 17:51:03 +0100

    successful and failed component in example
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- example/pages/index/+Page.vue
+++ example/pages/index/+Page.vue
@@ -3,27 +3,38 @@
   This page is:
   <ul>
     <li>Rendered to HTML.</li>
-    <li>Interactive.
-      <ClientOnly :load="load">
-        <template #fallback>
-          Loading...
-        </template>
-        <template #error>
-          Error loading component
-        </template>
-      </ClientOnly>
-    </li>
+    <li>Interactive.</li>
   </ul>
+  <p>Successful Client Only component</p>
+  <ClientOnly :load="load">
+    <template #fallback>
+      Loading...
+    </template>
+  </ClientOnly>
+  <p>Failed Client Only component</p>
+  <ClientOnly :load="fail">
+    <template #fallback>
+      Loading...
+    </template>
+    <template #error>
+      Error loading component
+    </template>
+  </ClientOnly>
 </template>
 
 <script lang="ts" setup>
 import { ClientOnly } from "../../../src"
-import Counter from '../../components/Counter.vue';
 
 // const load = () => import("../../components/Counter.vue");
 const load = () => new Promise((resolve, reject) => {
+  setTimeout(async () => {
+    const Counter = await import("../../components/Counter.vue")
+    resolve(Counter)
+  }, 1000)
+})
+
+const fail = () => new Promise((resolve, reject) => {
   setTimeout(() => {
-    // resolve(Counter)
     reject()
   }, 1000)
 })
